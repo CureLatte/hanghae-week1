5f2e200fb6626da86b5a0ea6dc305908d5f87748
zhqmfkv <zhqmfkv@naver.com>
2024-09-22 17:10:09 +0900

    [FEAT] 프로젝트 등록

---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,21 @@
+package io.hhplus.tdd.point;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PointService {
+
+
+
+    // DI
+    public PointService() {
+
+    }
+
+    public PointHistory getUserPointById(long id){
+
+
+
+    }
+
+}
