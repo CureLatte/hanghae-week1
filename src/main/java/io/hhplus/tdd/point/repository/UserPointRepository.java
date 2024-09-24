c438884c12d0070e848fc5e796c3dac93cf23ec7
zhqmfkv <zhqmfkv@naver.com>
2024-09-24 23:25:21 +0900

    feat: service, repository 인터페이스 생성

---
+++ src/main/java/io/hhplus/tdd/point/repository/UserPointRepository.java
@@ -0,0 +1,12 @@
+package io.hhplus.tdd.point.repository;
+
+import io.hhplus.tdd.point.record.UserPoint;
+
+public interface UserPointRepository {
+
+    // point 조회
+    public UserPoint findById(long id);
+
+
+
+}
