e4a7f12cee665eafed52ef45e241825777d92995
zhqmfkv <zhqmfkv@naver.com>
2024-09-25 20:42:29 +0900

    feat: 최대 포인트, 최대 충전 포인트 단위테스트 추가

--- src/test/java/io/hhplus/tdd/point/service/pointService/MethodChargePointById.java
+++ src/test/java/io/hhplus/tdd/point/service/pointService/MethodChargePointById.java
@@ -5,6 +5,8 @@ import io.hhplus.tdd.point.record.UserPoint;
 import io.hhplus.tdd.point.type.TransactionType;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
@@ -15,7 +17,7 @@ public class MethodChargePointById extends TestPointService {
     }
 
     @Test
-    public void ID_음수인_경우(){
+    public void ID_음수인_경우() {
         // GIVEN
         long userId = -999;
         long amount = 100;
@@ -46,6 +48,38 @@ public class MethodChargePointById extends TestPointService {
         assertEquals(err.getMessage(), this.pointService.AMOUNT_VALIDATOR_ERROR_MESSAGE);
     }
 
+    @Test
+    public void 최대_충전_금액_이상_충전시_에러() {
+        // GIVEN
+        long userId = 1;
+        long chargePoint = this.pointService.MAX_CHARGE_POINT + 1;
+
+        // WHEN
+        Exception err = assertThrows(IllegalArgumentException.class, () -> {
+            this.pointService.chargePointById(userId, chargePoint);
+        });
+
+
+        // THEN
+        assertEquals(err.getMessage(), this.pointService.MAX_CHARGE_POINT_VALIDATOR_ERROR_MESSAGE);
+    }
+
+    @Test
+    public void 최대_잔고_이상_충전시_에러() {
+        // GIVEN
+        long userId = 1;
+        long chargePoint = 1;
+
+        when(this.userPointRepository.findById(userId)).thenReturn(new UserPoint(userId, this.pointService.MAX_POINT, 0));
+
+        // WHEN
+        Exception err = assertThrows(IllegalArgumentException.class, () -> {
+            this.pointService.chargePointById(userId, chargePoint);
+        });
+
+        // THEN
+        assertEquals(err.getMessage(), this.pointService.MAX_POINT_VALIDATOR_ERROR_MESSAGE);
+    }
 
 
 }
