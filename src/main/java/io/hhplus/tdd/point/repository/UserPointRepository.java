4f375d8b657eab6b514f1ae4f58ac4af8eccaef8
zhqmfkv <zhqmfkv@naver.com>
2024-09-25 18:04:14 +0900

    feat: userPointRepository 포인트 올리기 메소드 추가 및 구현

--- src/main/java/io/hhplus/tdd/point/repository/UserPointRepository.java
+++ src/main/java/io/hhplus/tdd/point/repository/UserPointRepository.java
@@ -3,9 +3,8 @@ package io.hhplus.tdd.point.repository;
 import io.hhplus.tdd.point.record.UserPoint;
 
 public interface UserPointRepository {
-
     // point 조회
     public UserPoint findById(long id);
-
+    public UserPoint increasePoint(long id, long amount);
 
 }
