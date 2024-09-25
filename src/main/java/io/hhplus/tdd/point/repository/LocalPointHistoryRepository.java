799e3b3bec7ba6afe98fc6f4447a361e6abd9397
zhqmfkv <zhqmfkv@naver.com>
2024-09-25 18:03:22 +0900

    feat: pointHistoryRepository 충전내역 생성 메소드 구현

--- src/main/java/io/hhplus/tdd/point/repository/LocalPointHistoryRepository.java
+++ src/main/java/io/hhplus/tdd/point/repository/LocalPointHistoryRepository.java
@@ -2,6 +2,7 @@ package io.hhplus.tdd.point.repository;
 
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.point.record.PointHistory;
+import io.hhplus.tdd.point.type.TransactionType;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,5 +21,15 @@ public class LocalPointHistoryRepository implements PointHistoryRepository {
         return this.pointHistoryTable.selectAllByUserId(id);
     }
 
+    /** 사용 포인트 내역 생성
+     * @param userId long
+     * @param amount long
+     * @return PointHistory
+     */
+    @Override
+    public PointHistory createChargePoint(long userId, long amount) {
+        long nowMils = System.currentTimeMillis();
+        return this.pointHistoryTable.insert(userId, amount, TransactionType.USE, nowMils);
+    }
 
 }
