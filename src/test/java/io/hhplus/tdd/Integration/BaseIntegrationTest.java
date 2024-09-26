225b9f51c2a390931c6ab9312824bb6f4ac886d3
zhqmfkv <zhqmfkv@naver.com>
2024-09-27 00:16:47 +0900

    feat: 통합 테스트 작성

---
+++ src/test/java/io/hhplus/tdd/Integration/BaseIntegrationTest.java
@@ -0,0 +1,24 @@
+package io.hhplus.tdd.Integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@Disabled
+@AutoConfigureMockMvc
+public class BaseIntegrationTest {
+
+    @Autowired
+    protected MockMvc mvc;
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+
+}
--- src/test/java/io/hhplus/tdd/Integration/point/TestPointService.java
+++
@@ -1,4 +0,0 @@
-package io.hhplus.tdd.Integration.point;
-
-public class TestPointService {
-}
