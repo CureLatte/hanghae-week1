e5733be1f58c4b74bdb8c492ec7b80141869ac3d
zhqmfkv <zhqmfkv@naver.com>
2024-09-26 19:43:54 +0900

    feat: userPository 구현

--- src/main/java/io/hhplus/tdd/point/domain/repository/IUserPointRepository.java
+++ src/main/java/io/hhplus/tdd/point/domain/repository/IUserPointRepository.java
@@ -1,4 +1,8 @@
 package io.hhplus.tdd.point.domain.repository;
 
+import io.hhplus.tdd.point.domain.entity.Point;
+
 public interface IUserPointRepository {
+    public Point findById(long id);
+    public Point save(Point point);
 }
